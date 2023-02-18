ebef1d4a8139ccbc7540964c7c7589a86fcbc71a
bibeltr <125701527+bibeltr@users.noreply.github.com>
2023-02-18 15:45:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
-// Get the bible data from the json file
-let bibleData = fetch('bibel.json').then(res => res.json());
+const bookSelect = document.querySelector('#bibleBook');
+const chapterSelect = document.querySelector('#bibleChapter');
+const versesList = document.querySelector('#verses');
 
-// When the data is loaded, populate the select element with the bible books
-bibleData.then(data => {
-  let select = document.querySelector('#bibleBook');
-  let chaptersList = document.querySelector('#chapters');
+// Load bible data
+fetch('bibel.json')
+  .then(res => res.json())
+  .then(data => {
+    // Populate the book select
+    data.forEach(book => {
+      const bookOption = document.createElement('option');
+      bookOption.value = book.book_number;
+      bookOption.textContent = book.book_name;
+      bookSelect.appendChild(bookOption);
+    });
 
+    // Set the initial book and chapter to load
+    let selectedBookNumber = bookSelect.value;
+    let selectedChapterNumber = 1;
 
-  data.forEach(book => {
-    let bookOption = document.createElement('option');
-    bookOption.value = book.book_number;
-    bookOption.innerHTML = `${book.book_name}`;
-    select.appendChild(bookOption);
-  });
-
-  // When a book is selected, populate the chapters list with the corresponding chapters
-  select.addEventListener('change', event => {
-    let bookNumber = event.target.value;
-    let book = data.find(book => book.book_number == bookNumber);
-
+    // Populate the chapter select with the first chapter
+    const book = data.find(book => book.book_number == selectedBookNumber);
     if (book) {
-      // Clear the previous chapters list
-      while (chaptersList.firstChild) {
-        chaptersList.removeChild(chaptersList.firstChild);
-      }
-
-      // Populate the chapters list
       book.chapters.forEach(chapter => {
-        let chapterItem = document.createElement('li');
-        let chapterLink = document.createElement('a');
-        chapterLink.href = '#';
-        chapterLink.innerHTML = chapter.chapter_number;
-        chapterItem.appendChild(chapterLink);
-        chaptersList.appendChild(chapterItem);
-
-        // Add a hover effect to the chapter link
-        chapterLink.addEventListener('mouseover', () => {
-          chapterLink.style.textDecoration = 'underline';
-        });
-        chapterLink.addEventListener('mouseout', () => {
-          chapterLink.style.textDecoration = 'none';
-        });
-
-        // When a chapter is clicked, populate the verses list
-        chapterLink.addEventListener('click', () => {
-          let versesList = document.querySelector('#verses');
-          let versesHTML = '';
-
-          // Display the book name and chapter number above the verses
-          versesHTML += `<h3>${book.book_name} ${chapter.chapter_number}</h3>`;
-
-          chapter.verses.forEach(verse => {
-            versesHTML += `<p><strong style="color: #0051ff;">${book.book_name} ${chapter.chapter_number}:${verse.verse_number}</strong> ${verse.verse_text}</p>`;
-          });
-
-          versesList.innerHTML = versesHTML;
-        });
-
-        // Automatically select the first chapter of the book
-        if (chapter.chapter_number == 1) {
-          chapterLink.click();
-        }
+        const chapterOption = document.createElement('option');
+        chapterOption.value = chapter.chapter_number;
+        chapterOption.textContent = `Kapitel ${chapter.chapter_number}`;
+        chapterSelect.appendChild(chapterOption);
       });
     }
+
+    // Load the first chapter verses on page load
+    loadChapterVerses(selectedBookNumber, selectedChapterNumber, data);
+
+    // When a book is selected, populate the chapter select with the corresponding chapters
+    bookSelect.addEventListener('change', event => {
+      const bookNumber = event.target.value;
+      const book = data.find(book => book.book_number == bookNumber);
+      chapterSelect.innerHTML = '';
+      if (book) {
+        book.chapters.forEach(chapter => {
+          const chapterOption = document.createElement('option');
+          chapterOption.value = chapter.chapter_number;
+          chapterOption.textContent = `Kapitel ${chapter.chapter_number}`;
+          chapterSelect.appendChild(chapterOption);
+        });
+        selectedBookNumber = bookNumber;
+        selectedChapterNumber = 1;
+        loadChapterVerses(selectedBookNumber, selectedChapterNumber, data);
+      }
+    });
+
+    // When a chapter is selected, load the corresponding verses
+    chapterSelect.addEventListener('change', event => {
+      const chapterNumber = event.target.value;
+      selectedChapterNumber = chapterNumber;
+      loadChapterVerses(selectedBookNumber, selectedChapterNumber, data);
+    });
   });
-});
 
+// Function to load the verses of a chapter and display them on the page
+function loadChapterVerses(bookNumber, chapterNumber, data) {
+  const book = data.find(book => book.book_number == bookNumber);
+  if (book) {
+    const chapter = book.chapters.find(chapter => chapter.chapter_number == chapterNumber);
+    if (chapter) {
+      let versesHTML = `<h3>${book.book_name} ${chapter.chapter_number}</h3>`;
+      chapter.verses.forEach(verse => {
+        versesHTML += `<p><strong>${book.book_name} ${chapter.chapter_number}:${verse.verse_number}</strong> ${verse.verse_text}</p>`;
+      });
+      versesList.innerHTML = versesHTML;
+    }
+  }
+}
 
-        // Funktionen für die Webseite
+  // Funktionen für die Webseite
 
         // Nachoben-Button
 
-document.querySelector('#toTopButton').addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+        document.querySelector('#toTopButton').addEventListener('click', () => {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
